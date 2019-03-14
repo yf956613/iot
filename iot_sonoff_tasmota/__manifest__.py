6c217abe6e8a1c9a97f6533169926f48fae1d35c
Enric Tobella <etobella@creublanca.es>
2019-03-14 18:34:13 +0100

    [ADD] base_iot & Co

---
+++ iot_sonoff_tasmota/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'IoT Templates',
+    'version': '11.0.1.0.0',
+    'category': 'IoT',
+    'author': "Creu Blanca, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'installable': True,
+    'summary': 'IoT base module',
+    'depends': [
+        'iot_template',
+        'iot_input_amqp',
+    ],
+    'data': [
+        'data/template_data.xml',
+    ],
+}
